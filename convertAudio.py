006ddb0065f13ee3fcdfb8f0798791bbd7129cc4
Chris Palmer <capalmer1013@gmail.com>
2016-02-27 10:43:40 -0500

    spaces

--- convertAudio.py
+++ convertAudio.py
@@ -3,13 +3,13 @@ import numpy
 
 def str_to_int(s):
     i = int(s, 16)
-    if i >= 2**23:
-        i -= 2**24
+    if i >= 2**7:
+        i -= 2**8
     return i
 
 
 def int_to_str(i):
-    return '%06x'%((i+2**24)%2**24)
+    return '%06x' % ((i+2**24) % 2**24)
 
 
 def convertWaveFile(waveFile, filename):
@@ -25,7 +25,8 @@ def convertWaveFile(waveFile, filename):
     byteString = waveFile.readframes(numberOfFrames)
     # byteArray = [elem.encode("hex") for elem in byteString]
     for elem in byteString:
-        print str_to_int(elem.encode("hex"))
+        print elem.encode("hex")
+        print float(str_to_int(elem.encode("hex")))
     # print byteArray
     print waveFile.getnframes()
     print waveFile.getsampwidth()
